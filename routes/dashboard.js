bf46169aa07c47b911922a657aa757c4a9f02ad2
LuDeOliv <ludeoliv@ie.ibm.com>
2017-11-28 09:57:39 +0000

    Fixed #23 , #24

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,8 +4,8 @@ let router = express.Router();
 /* GET dashboard page. */
 router.get('/', function(req, res, next) {
     if(req.user){
-
-        res.render('./dashboard/dashboard',  {user: req.user});
+        req.user.filelist.forEach(d => console.log(d.name));
+        res.render('./dashboard/dashboard',  {user: req.user, 'fileList': req.user.filelist});
     } else {
         res.redirect('/singing');
     }
